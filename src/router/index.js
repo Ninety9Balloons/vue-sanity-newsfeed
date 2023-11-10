588183fc072ff364af85bbb84b87bf324bb2280e
Troy <tdreuther@gmail.com>
2023-11-10 14:36:31 -0500

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,41 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import store from "../store";
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    {
+        path: "/",
+        name: "home",
+        component: HomeView,
+    },
+    {
+        path: "/authors",
+        name: "authors",
+        component: () => import("../views/AuthorsView.vue"),
+    },
+    {
+        path: "/post/:id",
+        name: "post",
+        component: () => import("../views/post/_id.vue"),
+    },
+    {
+        path: "/author/:id",
+        name: "author",
+        component: () => import("../views/author/_id.vue"),
+    },
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+});
 
-export default router
+router.afterEach((to, from) => {
+    if (from.name) {
+        document.documentElement.scrollTop = 0;
+
+        store.dispatch("CloseMenu");
+    }
+});
+
+export default router;
